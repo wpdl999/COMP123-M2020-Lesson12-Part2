5946c4f341e6a1cdcdff9a444eafde1fabfb3e66
Wooram Moon <wpdl555@naver.com>
2020-08-12 11:55:08 -0400

    Add project files.

---
+++ COMP123-M2020-Lesson12-Part2/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_Lesson12_Part2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
